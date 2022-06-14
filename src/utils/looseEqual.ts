cdaf2df28bfb47d43ae33ea451d421ce1b640476
allen <272970666@qq.com>
2022-06-15 04:31:44 +0800

    feat: update utils

---
+++ src/utils/looseEqual.ts
@@ -0,0 +1,54 @@
+import { isArray, isDate, isObject, isSymbol } from './is'
+
+function looseCompareArrays(a: any[], b: any[]) {
+  if (a.length !== b.length) return false
+  let equal = true
+  for (let i = 0; equal && i < a.length; i++) {
+    equal = looseEqual(a[i], b[i])
+  }
+  return equal
+}
+
+export function looseEqual(a: any, b: any): boolean {
+  if (a === b) return true
+  let aValidType = isDate(a)
+  let bValidType = isDate(b)
+  if (aValidType || bValidType) {
+    return aValidType && bValidType ? a.getTime() === b.getTime() : false
+  }
+  aValidType = isSymbol(a)
+  bValidType = isSymbol(b)
+  if (aValidType || bValidType) {
+    return a === b
+  }
+  aValidType = isArray(a)
+  bValidType = isArray(b)
+  if (aValidType || bValidType) {
+    return aValidType && bValidType ? looseCompareArrays(a, b) : false
+  }
+  aValidType = isObject(a)
+  bValidType = isObject(b)
+  if (aValidType || bValidType) {
+    /* istanbul ignore if: this if will probably never be called */
+    if (!aValidType || !bValidType) {
+      return false
+    }
+    const aKeysCount = Object.keys(a).length
+    const bKeysCount = Object.keys(b).length
+    if (aKeysCount !== bKeysCount) {
+      return false
+    }
+    for (const key in a) {
+      const aHasKey = Object.prototype.hasOwnProperty.call(a, key)
+      const bHasKey = Object.prototype.hasOwnProperty.call(b, key)
+      if ((aHasKey && !bHasKey) || (!aHasKey && bHasKey) || !looseEqual(a[key], b[key])) {
+        return false
+      }
+    }
+  }
+  return String(a) === String(b)
+}
+
+export function looseIndexOf(arr: any[], val: any): number {
+  return arr.findIndex((item) => looseEqual(item, val))
+}
